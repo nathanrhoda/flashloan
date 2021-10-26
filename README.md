45a0405e330ad299dc03078fa8f9e21fa25208ae
Nathan Rhoda <nathan.rhoda@hotmail.com>
2021-10-26 22:07:34 +0200

    extended to flashloan to configure amount and updated addresses

--- README.md
+++ README.md
@@ -3,3 +3,19 @@ Running through truffle box flashloan tutorial
 
 
 https://www.trufflesuite.com/boxes/flashloan-box
+
+
+https://docs.aave.com/developers/v/1.0/deployed-contracts/deployed-contract-instances
+
+DAI address => 0xf80a32a835f79d7787e8a8ee5721d0feafd78108
+
+
+Faucet => https://faucet.dimensions.network/
+
+contract address => 0x5244262e7b9Ed382C8CE5FF0944044Fb34FF98d9
+
+Script to do flashloan
+- let f = await Flashloan.deployed()
+- asset = '0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD'
+- amount = web3.utils.toWei('1000')
+- f.flashloan(asset, amount)
--- src/migrations/2_deploy_contracts.js
+++ src/migrations/2_deploy_contracts.js
@@ -11,9 +11,11 @@ module.exports = async function (deployer, network) {
             case "development": // For Ganache mainnet forks
                 lendingPoolAddressesProviderAddress = "0x24a42fD28C976A61Df5D00D0599C34c4f90748c8"; break
             case "ropsten":
+                lendingPoolAddressesProviderAddress = "0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728"; break
             case "ropsten-fork":
                 lendingPoolAddressesProviderAddress = "0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728"; break
-            case "kovan":
+            case "kovan":                
+                lendingPoolAddressesProviderAddress = "0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5"; break
             case "kovan-fork":
                 lendingPoolAddressesProviderAddress = "0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5"; break
             default:
